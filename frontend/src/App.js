e65119be6ffee756c65cfcb463e00d583835dc0d
abdulxaqdev <abdulkhak8tursunov@gmail.com>
2022-09-30 17:42:25 +0500

    fix bug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import Header from "./components/Header";
 import Data from "./components/Data";
 import { faker } from "@faker-js/faker";
@@ -28,10 +27,19 @@ function App() {
  };
 
  function insertError(data) {
-  const r = error
+  const r = error;
   let rs = ["a", "s", "d"];
-  let re = rs[Math.floor(Math.random()*3)];
+  let re = rs[Math.floor(Math.random() * 3)];
   let errorData = data;
+  function sw(str, first, last) {
+   return (
+    str.substr(0, first) +
+    str[last] +
+    str.substring(first + 1, last) +
+    str[first] +
+    str.substr(last + 1)
+   );
+  }
 
   switch (re) {
    case "d":
@@ -48,15 +56,6 @@ function App() {
       errorData = sw(data, r, r + 1);
      }
     }
-    function sw(str, first, last) {
-     return (
-      str.substr(0, first) +
-      str[last] +
-      str.substring(first + 1, last) +
-      str[first] +
-      str.substr(last + 1)
-     );
-    }
     break;
   }
 
@@ -68,23 +67,16 @@ function App() {
   faker.seed(Number(r));
   let all = [];
   let dataNum = p === 1 ? 20 : (p + 1) * 10;
-  console.log(error, typeof error);
   for (let i = 0; i < dataNum; i++) {
    all.push([
     error === 0
      ? faker.random.numeric(5)
      : insertError(faker.random.numeric(5)),
-    error === 0
-     ? faker.name.fullName()
-     : insertError(faker.name.fullName()),
+    error === 0 ? faker.name.fullName() : insertError(faker.name.fullName()),
     error === 0
      ? `${faker.address.city()}, ${faker.address.street()}`
-     : insertError(
-        `${faker.address.city()}, ${faker.address.street()}`
-       ),
-    error === 0
-     ? faker.phone.number()
-     : insertError(faker.phone.number()),
+     : insertError(`${faker.address.city()}, ${faker.address.street()}`),
+    error === 0 ? faker.phone.number() : insertError(faker.phone.number()),
    ]);
   }
   setAllData([...all]);
