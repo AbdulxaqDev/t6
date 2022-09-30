ea7072212ed271bf0944e769f4a785560518538d
abdulxaqdev <abdulkhak8tursunov@gmail.com>
2022-09-30 16:30:34 +0500

    add feature of insertimng error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ import Data from "./components/Data";
 import { faker } from "@faker-js/faker";
 import { useEffect, useState } from "react";
 
-
 function App() {
  const [country, setCountry] = useState("fr");
  const [rnn, setRnn] = useState("");
@@ -12,63 +11,96 @@ function App() {
  const [allData, setAllData] = useState([]);
  const [page, setPage] = useState(1);
 
- const nextTen = (p) => { 
-  setPage(page + 1)
-  // generateData(rnn, country)
- }
+ const nextTen = (p) => {
+  setPage(page + 1);
+ };
 
  const selectedCountry = (c) => {
-  generateData(rnn, c);
+  generateData(rnn, c, page);
   setCountry(c);
  };
  const randomNumber = (r) => {
-  generateData(r, country);
+  generateData(r, country, page);
   setRnn(r);
  };
  const errors = (e) => {
-  setError(e);
+  console.log("hrllo");
+  setError(parseFloat(e));
  };
 
- function generateData(r, c) {
+ function insertError(data) {
+  const r = error
+  let rs = ["a", "s", "d"];
+  let re = rs[Math.floor(Math.random()*3)];
+  let errorData = data;
+
+  switch (re) {
+   case "d":
+    errorData = data.substring(0, r - 1) + data.substring(r, data.length);
+    break;
+   case "a":
+    errorData = data.slice(0, r) + r + data.slice(r);
+    break;
+   case "s":
+    if (data.length > 1) {
+     if (r == data.length - 1) {
+      errorData = sw(data, r, r - 1);
+     } else {
+      errorData = sw(data, r, r + 1);
+     }
+    }
+    function sw(str, first, last) {
+     return (
+      str.substr(0, first) +
+      str[last] +
+      str.substring(first + 1, last) +
+      str[first] +
+      str.substr(last + 1)
+     );
+    }
+    break;
+  }
+
+  return errorData;
+ }
+
+ console.log(insertError("Hello"));
+
+ function generateData(r, c, p) {
   faker.locale = c;
   faker.seed(Number(r));
   let all = [];
-  let dataNum = page === 1 ? 20 : (page + 1) * 10;
+  let dataNum = p === 1 ? 20 : (p + 1) * 10;
+  console.log(error, typeof error);
   for (let i = 0; i < dataNum; i++) {
    all.push([
-    faker.random.numeric(5),
-    faker.name.fullName(),
-    `${faker.address.city()}, ${faker.address.street()}`,
-    faker.phone.number(),
+    error === 0
+     ? faker.random.numeric(5)
+     : insertError(faker.random.numeric(5)),
+    error === 0
+     ? faker.name.fullName()
+     : insertError(faker.name.fullName()),
+    error === 0
+     ? `${faker.address.city()}, ${faker.address.street()}`
+     : insertError(
+        `${faker.address.city()}, ${faker.address.street()}`
+       ),
+    error === 0
+     ? faker.phone.number()
+     : insertError(faker.phone.number()),
    ]);
   }
   setAllData([...all]);
  }
- 
+
  useEffect(() => {
   if (page > 1) {
-   generateData(rnn, country, page)
+   generateData(rnn, country, page);
   }
- }, [page, error])
-
- const deleteError = (d) => {
-  let r = Math.floor(Math.random() * d.length)
-  d.splice(r, 1)
- }
- const addError = () => { }
- const swapError = () => { }
-
- const insertError = (e) => { 
-  let wrongDatas = allData.length * e
-  for (let i = 0; i < wrongDatas; i++) {
-   let r = Math.floor(Math.random() * allData)
-   
+  if (error > 0) {
+   generateData(rnn, country, page);
   }
- }
-
-
-
-
+ }, [page, error]);
 
  return (
   <div className="App" style={{ padding: "100px 50px " }}>
@@ -76,7 +108,7 @@ function App() {
     selectedCountry={selectedCountry}
     randomNumber={randomNumber}
     errors={errors}
-    allData={ allData}
+    allData={allData}
    />
    <Data dataNum={allData} nextTen={nextTen} />
   </div>
